b9b90edb12114355a5854f86421f9a6147994b76
Jessica  Rattani <jjrattani@gmail.com>
2020-07-03 02:22:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,3 +16,9 @@ def list_dwarves(array)
     puts 
   
 end
+
+hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
+hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
